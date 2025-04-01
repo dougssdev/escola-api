0f30c5ef1d9c897c14c39f7365dd6f07399fe3c6
dougssdev <douglassabinosilva@hotmail.com>
2025-04-01 09:51:15 -0300

    Iniciando o projeto

---
+++ src/test/java/br/com/escolaapi/EscolaapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.escolaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscolaapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
